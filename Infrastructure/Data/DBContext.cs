567804b36fedbe4c9f1a3338c34f8ca1c5b9f3a6
HemantKatkar <49052217+HemantKatkar@users.noreply.github.com>
2019-03-29 10:24:34 +0530

    Infrastructure
    Db Repository

---
+++ Infrastructure/Data/DBContext.cs
@@ -0,0 +1,36 @@
+﻿namespace Infrastructure.Data
+{
+    using ApplicationCore.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class DBContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DBContext"/> class.
+        /// </summary>
+        /// <param name="options">The options.</param>
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Override this method to further configure the model that was discovered by convention from the entity types
+        /// exposed in <see cref="T:Microsoft.EntityFrameworkCore.DbSet`1" /> properties on your derived context. The resulting model may be cached
+        /// and re-used for subsequent instances of your derived context.
+        /// </summary>
+        /// <param name="modelBuilder">The builder being used to construct the model for this context. Databases (and other extensions) typically
+        /// define extension methods on this object that allow you to configure aspects of the model that are specific
+        /// to a given database.</param>
+        /// <remarks>
+        /// If a model is explicitly set on the options for this context (via <see cref="M:Microsoft.EntityFrameworkCore.DbContextOptionsBuilder.UseModel(Microsoft.EntityFrameworkCore.Metadata.IModel)" />)
+        /// then this method will not be run.
+        /// </remarks>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Items>();
+            modelBuilder.Entity<Options>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Infrastructure/Data/EfRepository.cs
@@ -0,0 +1,54 @@
+﻿// <copyright file="EfRepository.cs" company="Tatvasoft">
+// Copyright (c) Tatvasoft. All rights reserved.
+// </copyright>
+
+namespace Infrastructure.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using ApplicationCore.Common;
+    using ApplicationCore.Interfaces;
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// class EfRepository
+    /// </summary>
+    public sealed class EfRepository<T> : IRepository<T>
+        where T : class
+    {
+        private readonly DBContext dbContext;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EfRepository{T}"/> class.
+        /// </summary>
+        /// <param name="dbContext">The database context.</param>
+        public EfRepository(DBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// Executes the with store procedure.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>returns result of stored procedure</returns>
+        public IQueryable<T> ExecWithStoreProcedure(string query, params object[] parameters)
+        {
+            return this.dbContext.Set<T>().FromSql(query, parameters);
+        }
+
+        /// <summary>
+        /// Executes the stored procedure list.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="sql">The SQL.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>returns result of stored procedures</returns>
+        public IQueryable<TEntity> ExecuteStoredProcedureList<TEntity>(string sql, params object[] parameters)
+            where TEntity : class
+            => this.dbContext.Set<TEntity>().FromSql(sql, parameters);
+    }
+}
